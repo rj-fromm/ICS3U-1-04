9d49ce777b730cbfa6812cdc6cda3c18ce51ca50
Ubuntu <ubuntu@ip-172-31-25-71.ec2.internal>
2019-09-13 18:48:18 +0000

    Multiplicatation program

---
+++ Multiplication.py
@@ -0,0 +1,13 @@
+#!user/bin/env python3
+
+#Created by: RJ Fromm
+#Created on: September 2019
+#This is a Multiplication program
+
+
+def main():
+    print("Rectangle has length of 5cm, and width of 3cm")
+    print ("Area of rectangle is 5*3 ={}".format(5*3))
+    print ("Perimeter of rectangle is 5+5+3+3 ={}".format(5+5+3+3))
+if __name__ == "__main__": 
+    main()
---
+++ multiplication program.py
@@ -0,0 +1,17 @@
+#!user/bin/env python3
+
+#Created by: RJ Fromm
+#Created on: September 2019
+#This is a multiplcation program 
+
+
+print("Hello fellow user, what is ")
+
+first_number = int(input("Please for the love of Snakob enter first number : "))
+
+second_number = int(input("Please for the love of snakob enter the second number : "))
+
+sum_of_first_number_second_number = first_number * second_number
+
+print("The sum of {} and {} is : {}".format(first_number,second_number,sum_of_first_number_second_number))
+
